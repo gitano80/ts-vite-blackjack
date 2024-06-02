5dd418dcd81341d520ccfeed7b52c8088967a63d
Franco Blasi <francoblasi@hotmail.com>
2024-06-02 01:15:43 -0300

    crear carta modificado, ruta

--- src/Blackjack/usecases/create-card.ts
+++ src/Blackjack/usecases/create-card.ts
@@ -1,7 +1,7 @@
 export const crearCarta = (carta: string, place: HTMLDivElement): void => {
   const nuevaCarta: HTMLImageElement = document.createElement('img');
 
-  nuevaCarta.src = `/assets/cartas/${carta}.png`;
+  nuevaCarta.src = `./assets/cartas/${carta}.png`;
   nuevaCarta.classList.add('cartas');
   place.append(nuevaCarta);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
   // opciones de configuración
+  publicDir: 'public',
   base: "https://gitano80.github.io/ts-vite-blackjack/"
-}
+})
